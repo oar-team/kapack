98a98e264441419282914657f2f9e6ddce0efe99
Valentin Reis <valentin.reis@gmail.com>
2017-03-27 13:37:01 +0200

    ocs works.

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 {pkgs ? import <nixpkgs> {} }:
 let
   callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // self);
+  #ocamlCallPackage = pkgs.ocamlPackages.callPackageWith (pkgs // pkgs.xlibs // self);
   self = {
     simgrid_batsim = callPackage ./simgrid/batsim.nix { };
     batsim = callPackage ./batsim { };
@@ -8,7 +9,9 @@ let
     rapidjson = callPackage ./rapidjson { };
     obandit = pkgs.ocamlPackages.callPackage ./obandit { };
     zymake = pkgs.ocamlPackages.callPackage ./zymake { };
-    #ocs = pkgs.ocamlPackages.callPackage ./ocs { };
+    ocs = pkgs.ocamlPackages.callPackage ./ocs {
+      obandit =  pkgs.ocamlPackages.callPackage ./obandit { };
+    };
   };
 in
-self
+  self
