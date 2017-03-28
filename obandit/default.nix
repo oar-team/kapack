d0db5738e4ed9a7e8de4064fcc70ec8295d66f92
Valentin Reis <valentin.reis@gmail.com>
2017-03-28 09:18:25 +0200

    update url and description for obandit

--- obandit/default.nix
+++ obandit/default.nix
@@ -4,8 +4,8 @@ stdenv.mkDerivation rec {
 	name = "obandit-${version}";
 	version = "0.2.1";
 	src = fetchurl {
-		url = "http://freux.fr/obandit/releases/obandit-${version}.tbz";
-		sha256 = "168ffhqc1sqn9aphgdh7bfsy6i5i7f3fal3nmqhfcwbm0h5779zm";
+		url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
+		sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
 	};
 
 	unpackCmd = "tar xjf $src";
@@ -18,8 +18,8 @@ stdenv.mkDerivation rec {
 
 	meta = {
 		license = stdenv.lib.licenses.isc;
-		homepage = http://erratique.ch/software/rresult;
-		description = "Result value combinators for OCaml";
+		homepage = https://github.com/freuk/obandit;
+		description = "OCaml module for multi-armed bandits";
 		inherit (ocaml.meta) platforms;
 	};
 }
