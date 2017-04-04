152db58a4c140600047a45b1508d84c65ed4d9e0
Valentin Reis <valentin.reis@gmail.com>
2017-04-04 16:29:27 +0200

    ocs argument

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs-channels/archive/nixos-17.03.tar.gz) {} }:
+{pkgs ? import <nixpkgs> {} }:
 let
   callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // self);
   #ocamlCallPackage = pkgs.ocamlPackages.callPackageWith (pkgs // pkgs.xlibs // self);
@@ -11,13 +11,13 @@ let
     evalys = callPackage ./evalys { inherit interval_set; };
     obandit = pkgs.ocamlPackages.callPackage ./obandit { };
     zymake = pkgs.ocamlPackages.callPackage ./zymake { };
-    ocs = pkgs.ocamlPackages.callPackage ./ocs { inherit obandit; };
+    ocs = pkgs.ocamlPackages.callPackage ./ocs { };
     evalysEnv=pkgs.stdenv.mkDerivation {
       name = "evalysEnv";
       buildInputs = [ pkgs.python3 pkgs.python35Packages.matplotlib evalys  ];
-      shellHook = ''
-        python3 -i -c "import evalys;import matplotlib;matplotlib.use('Qt5Agg');from matplotlib import pyplot as plt"
-      '';
+      #shellHook = ''
+        #python3 -i -c "import evalys;import matplotlib;matplotlib.use('Qt5Agg');from matplotlib import pyplot as plt"
+      #'';
     };
   };
 in
