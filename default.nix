142a0ad33899c97d675f056b039a9b263b554952
Valentin Reis <valentin.reis@gmail.com>
2017-05-26 00:55:25 +0200

    refactor arguments to ocs

--- default.nix
+++ default.nix
@@ -14,12 +14,14 @@ let
     zymake = pkgs.ocamlPackages.callPackage ./zymake { };
     onanomsg = pkgs.ocamlPackages.callPackage ./onanomsg { };
     ppx_deriving_protobuf = pkgs.ocamlPackages.callPackage ./ppx_deriving_protobuf { };
-    ocs = pkgs.ocamlPackages.callPackage ./ocs { inherit obandit; };
+    ocs = pkgs.ocamlPackages.callPackage ./ocs {
+      inherit obandit ppx_deriving_protobuf onanomsg;
+    };
     evalysEnv = pkgs.stdenv.mkDerivation {
       name = "evalysEnv";
       buildInputs = [ pkgs.python3 pkgs.python35Packages.matplotlib evalys  ];
       shellHook = ''
-        python3 -i -c "import evalys;import matplotlib;matplotlib.use('Qt5Agg');from matplotlib import pyplot as plt"
+          python3 -i -c "import evalys;import matplotlib;matplotlib.use('Qt5Agg');from matplotlib import pyplot as plt"
       '';
     };
   };
