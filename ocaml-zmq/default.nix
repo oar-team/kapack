d5fa66330fea742ad43479eacf99c7207eca1837
Valentin Reis <valentin.reis@gmail.com>
2017-05-29 22:34:15 +0200

    fixed ocaml-zmq

---
+++ ocaml-zmq/default.nix
@@ -0,0 +1,17 @@
+{ stdenv, fetchurl, ocaml, findlib, ocamlbuild, opam, stdint, zeromq }:
+
+stdenv.mkDerivation rec {
+	name = "ocaml-zmq-${version}";
+	version = "1.2.0";
+	src = fetchurl {
+		url = "https://github.com/issuu/ocaml-zmq/archive/4.0-8.tar.gz";
+		sha256 = "10j6jrfinlwg1sph4hraidccznf29rvkivjx1ayglh5r5x9a0cbw";
+	};
+
+  configurePhase = '' ocaml setup.ml -configure --prefix $out '';
+  buildPhase = "ocaml setup.ml -build";
+  installPhase = "ocaml setup.ml -install";
+  createFindlibDestdir = true;
+
+	buildInputs = [ ocaml findlib ocamlbuild opam stdint zeromq ];
+}
