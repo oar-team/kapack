41b8215dadf8fd92743875c11021e405229fcd8c
Valentin Reis <valentin.reis@gmail.com>
2017-07-26 17:22:20 +0200

    oocvx bump to git

--- oocvx/default.nix
+++ oocvx/default.nix
@@ -1,25 +1,28 @@
-{ stdenv, fetchurl, ocaml, findlib, ocamlbuild, opam, ocaml_batteries, sexplib, csv, topkg }:
+{ stdenv, fetchFromGitHub, ocaml, findlib, ocamlbuild, opam, ocaml_batteries, sexplib, csv, topkg }:
 
 stdenv.mkDerivation rec {
-	name = "oocvx-${version}";
-	version = "0.0.1";
-	src = fetchurl {
-		url = "https://github.com/freuk/oocvx/archive/v${version}-zenodo.tar.gz";
-		sha256 = "e1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
-	};
+  name = "oocvx-${version}";
+  version = "git";
 
-	unpackCmd = "tar xjf $src";
+  src = fetchFromGitHub {
+    owner = "freuk";
+    repo = "oocvx";
+    rev = "d302906db464f86d118558c5239e8c38fc754bef";
+    sha256 = "1cv6ykbyj8f8b8zhj6zasdjric3lzy9kc07z10brs5jq8ik4hnrs";
+  };
 
-	buildInputs = [ ocaml findlib ocamlbuild topkg opam  ];
+  unpackCmd = "tar xjf $src";
+
+  buildInputs = [ ocaml findlib ocamlbuild topkg opam  ];
 
   propagatedBuildInputs = [ ocaml_batteries sexplib csv ];
 
-	inherit (topkg) buildPhase installPhase;
+  inherit (topkg) buildPhase installPhase;
 
-	meta = {
-		license = stdenv.lib.licenses.isc;
-		#homepage = https://github.com/freuk/oocvx;
-		description = "OCaml online convex optimization";
-		inherit (ocaml.meta) platforms;
-	};
+  meta = {
+    license = stdenv.lib.licenses.isc;
+    #homepage = https://github.com/freuk/oocvx;
+    description = "OCaml online convex optimization";
+    inherit (ocaml.meta) platforms;
+  };
 }
