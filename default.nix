3afa445f0c60c5ded8101e8c8be3ccad5120ce80
MERCIER Michael <michael.mercier@inria.fr>
2017-09-25 11:11:23 +0200

    Add batsim docker image

--- default.nix
+++ default.nix
@@ -19,21 +19,35 @@ let
     stdint = pkgs.ocamlPackages.callPackage ./stdint { };
     onanomsg = pkgs.ocamlPackages.callPackage ./onanomsg { inherit nanomsg bigstring; };
     ppx_deriving_protobuf = pkgs.ocamlPackages.callPackage ./ppx_deriving_protobuf { };
-    ocs = pkgs.ocamlPackages.callPackage ./ocs { 
+    ocs = pkgs.ocamlPackages.callPackage ./ocs {
       inherit obandit oocvx ppx_deriving_protobuf;
-      zmq=ocaml-zmq; 
+      zmq=ocaml-zmq;
     };
+
     help = pkgs.stdenv.mkDerivation {
       name = "help";
       buildInputs = [ ocs ];
     };
+
     evalysEnv = pkgs.stdenv.mkDerivation {
       name = "evalysEnv";
-      buildInputs = [ pkgs.python3 pkgs.python35Packages.matplotlib evalys  ];
+      buildInputs = [ pkgs.python3 pkgs.python36Packages.matplotlib evalys  ];
       shellHook = ''
           python3 -i -c "import evalys;import matplotlib;matplotlib.use('Qt5Agg');from matplotlib import pyplot as plt"
       '';
     };
+
+    batsim_git = batsim.overrideDerivation (attrs: rec {
+      version = "dev";
+      src = builtins.fetchTarball "https://gitlab.inria.fr/batsim/batsim/repository/master/archive.tar.gz";
+      doInstallCheck = false;
+      propagatedNativeBuildInputs = [ ];
+      # Add debug tools
+      # propagatedNativeBuildInputs = attrs.propagatedNativeBuildInputs ++ [ pkgs.cmake pkgs.bash ];
+    });
+
+    batsimImage = callPackage ./batsim/batsim-docker.nix {};
+    batsimDocker_git = batsimImage batsim_git null;
   };
 in
   self
