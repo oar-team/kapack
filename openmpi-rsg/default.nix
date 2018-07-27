f8a146e3b5eb0dbba15c2a41d6a340b06962ed2b
Millian Poquet <millian.poquet@inria.fr>
2018-07-27 17:28:37 +0200

    osmpi: separate copy from hack. use rsync for copy.

--- openmpi-rsg/default.nix
+++ openmpi-rsg/default.nix
@@ -1,62 +1,81 @@
-{ stdenv, openmpi, openmpi_rsg_plugins, python }:
+{ stdenv, openmpi, openmpi_rsg_plugins, python, rsync }:
 
 stdenv.mkDerivation rec {
 # name = "openmpi-3.1.1";
   name = "openmpi-rsg-u"; # BEWARE: Length must equal openmpi package's
   src = "${openmpi_rsg_plugins}"; # We have no source here...
 
-  nativeBuildInputs = [ python ];
+  nativeBuildInputs = [ python rsync ];
   buildInputs = [ openmpi openmpi_rsg_plugins ];
   installPhase = ''
-    mkdir -p $out/lib/openmpi
+    mkdir -p $TMP/out/lib/openmpi
 
-    # Retrieve OpenMPI binaries, hacking their hardcoded install path.
+    # Copy all OpenmMPI files into the build directory
+    rsync -a ${openmpi}/* $TMP/out --exclude ompi --exclude orte --exclude opal
+
+    # Hack the hardcoded install path in OpenMPI binaries.
     # This is done in python, for sanity's sake
+    export BUILDDIR=$TMP
     export OPENMPI=${openmpi}
     export OUT=$out
 
     python_cmd=$(cat <<-END
     import glob
     import os
+    import sys
+    from pathlib import Path
+    from stat import *
 
-    old_path = os.environ['OPENMPI']
-    new_path = os.environ['OUT']
+    builddir_path = os.environ['BUILDDIR']
+    openmpi_path = os.environ['OPENMPI']
+    out_path = os.environ['OUT']
 
-    files_to_copyhack = \
-        glob.glob(old_path + "/bin/**", recursive=True) + \
-        glob.glob(old_path + "/lib/**", recursive=True) + \
-        glob.glob(old_path + "/etc/**", recursive=True) + \
-        glob.glob(old_path + "/share/**", recursive=True)
+    if len(openmpi_path) != len(out_path):
+        raise Exception("OpenMPI path ({}) and out path ({}) should have equal length".format(
+            openmpi_path, out_path))
 
-    for filename in files_to_copyhack:
+    files_to_hack = glob.glob(builddir_path + "/out/**", recursive=True)
+
+    for filename in files_to_hack:
         # Only work on files
         if not os.path.isfile(filename):
             continue
+        if os.path.islink(filename):
+            continue
 
-        old_mode = os.stat(filename).st_mode
+        # Mark the file as writable if needed
+        old_mode = os.stat(filename)[ST_MODE]
+        changed_mode = False
+        if old_mode & S_IWUSR == 0:
+            print("Setting write permission to {}".format(filename))
+            changed_mode = True
+            os.chmod(filename, old_mode | S_IWUSR)
 
-        new_filename = filename.replace(old_path, new_path)
-        print('{old} -> {new}'.format(old=filename, new=new_filename))
+        print('hack {}: {} -> {}'.format(filename, openmpi_path, out_path))
 
-        with open(filename, 'rb') as f:
+        # Regular (non-directory, non-links) files
+        with open(filename, 'rb+') as f:
+            print('File opened!')
             # Just read the content
             content = f.read()
 
             # Hack the path into the file content
-            content = content.replace(old_path.encode('ascii'), new_path.encode('ascii'))
+            content = content.replace(openmpi_path.encode('ascii'), out_path.encode('ascii'))
 
-            # Create directory (recursively) if needed
-            os.makedirs(os.path.dirname(new_filename), exist_ok=True)
+            # Update content
+            f.seek(0, 0)
+            f.write(content)
 
-            # Write hacked content to the output file (in the nix store)
-            with open(os.open(new_filename, os.O_CREAT | os.O_WRONLY, old_mode), 'wb') as fw:
-                fw.write(content)
-            # TODO: handle links (ln) correctly
+        if changed_mode:
+            os.chmod(filename, old_mode)
     END
     )
     python -c "$python_cmd"
 
     # Copy plugins
-    cp ${openmpi_rsg_plugins}/lib/openmpi/* $out/lib/openmpi/
+    rsync -a ${openmpi_rsg_plugins}/lib/* $TMP/out/lib
+
+    # Copy the temporary output directory to the nix store
+    rsync -a $TMP/out/* $out
   '';
 }
