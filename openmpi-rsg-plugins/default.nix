eb1e0f22437dc2685b6b9e8d35199205af46048e
Millian Poquet <millian.poquet@inria.fr>
2018-07-27 18:00:30 +0200

    openmpi-rsg-plugins: patch binary calls, log++

--- openmpi-rsg-plugins/default.nix
+++ openmpi-rsg-plugins/default.nix
@@ -1,5 +1,12 @@
 { clangStdenv, fetchgit, cmake, simgrid, remote_simgrid,
-  boost, cppzmq, glibcLocales, openmpi, jansson }:
+  boost, cppzmq, glibcLocales, openmpi, jansson, which
+
+# Print btl logs?
+, printBtlLogs ? true
+
+# Print odls logs?
+, printOdlsLogs ? true
+}:
 
 clangStdenv.mkDerivation rec {
   name = "openmpi-rsg-plugins-${version}";
@@ -11,17 +18,32 @@ clangStdenv.mkDerivation rec {
     sha256 = "1yyzdv1995lizl7n85i0d2ibkxslx4cpjkncwgkkfdkjc9fhkgqw";
   };
 
-  nativeBuildInputs = [ remote_simgrid boost cmake jansson openmpi ];
+  nativeBuildInputs = [ remote_simgrid boost cmake jansson openmpi which ];
   buildInputs = [ glibcLocales ];
-
-  # Not expected to compile yet
-  cmakeFlags = ["-DCMAKE_BUILD_TYPE=Debug"];
 
   # Environment variables used by cmake (convenient for shell)
   SimGrid_PATH = "${simgrid}";
   RSG_INSTALL_PATH = "${remote_simgrid}";
   OMPI_SRC_PATH = "${openmpi}";
   OMPI_INSTALL_PATH = "${openmpi}";
+
+  # Make sure plugins call the right RSG binaries
+  patchPhase = ''
+    # Patch rsg_server binary calls
+    sed -i -E \
+      "sW(const char \* rsgserver = \")(rsg_server)(\";)W\1$(which rsg_server)\3W" \
+      src/odls/rsg/odls_rsg_module.c
+
+    # Patch rsgstat binary calls
+    sed -i -E \
+      "sW(const char \* rsgstat = \")(rsgstat)(\";)W\1$(which rsgstat)\3W" \
+      src/odls/rsg/odls_rsg_module.c
+  '';
+
+  cmakeFlags = with clangStdenv; ["-DCMAKE_BUILD_TYPE=Debug"]
+    ++ lib.optional printBtlLogs "-Denable_debug_btl_self=ON"
+    ++ lib.optional printOdlsLogs "-Denable_debug_odls_rsg=ON"
+  ;
 
   installPhase = ''
     # Put all plugins into the lib/openmpi/ directory
