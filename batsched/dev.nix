db79868a7ae021ba66eeaac15b43fb6ab2c096c7
Millian Poquet <millian.poquet@inria.fr>
2018-08-30 23:14:46 +0200

    batsched_dev: add intervalset dependency

--- batsched/dev.nix
+++ batsched/dev.nix
@@ -1,9 +1,9 @@
-{ batsched }:
+{ batsched, intervalset }:
 
 batsched.overrideAttrs (oldAttrs: rec {
   name = "batsched-${version}";
   version = "git";
 
   src = fetchTarball "https://gitlab.inria.fr/batsim/batsched/repository/master/archive.tar.gz";
-  }
-)
+  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ intervalset ];
+})
