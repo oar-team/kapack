d9784d96e5f5c824e6f4c7aad3acc0008568f3be
MERCIER Michael <michael.mercier@inria.fr>
2018-08-31 17:03:24 +0200

    pybatsim: working --version

--- pybatsim/default.nix
+++ pybatsim/default.nix
@@ -1,16 +1,18 @@
-{ stdenv, pythonPackages, fetchurl, procset }:
+{ stdenv, pythonPackages, procset }:
 
 pythonPackages.buildPythonPackage rec {
-    name = "pybatsim-2.1";
+    pname = "pybatsim";
+    version = "2.1.1";
 
-    src = fetchurl {
-      url = "https://files.pythonhosted.org/packages/75/b9/a19f5b18349221185ade8c7b21f25800b3094f5d13f3af16d8c08c4ab220/pybatsim-2.1.tar.gz";
-      sha256 = "1qf618vc57x3fbxzm73ya87j98dx5mjpijlwsxbsjlh4kpr153ym";
+    src = pythonPackages.fetchPypi {
+      inherit pname version;
+      sha256 = "0awm4yamfqpmd5mb85xsrz1ip56pam1rrn0w9m87lr7viq9vmcip";
     };
 
     buildInputs = with pythonPackages; [
       autopep8
       coverage
+      ipdb
     ];
     propagatedBuildInputs = with pythonPackages; [
       sortedcontainers
