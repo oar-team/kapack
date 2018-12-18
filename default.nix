ba5f46e1922715b194f1f3646500ad8b7f214699
MERCIER Michael <michael.mercier@inria.fr>
2018-12-18 11:23:37 +0100

    [fix] my_lib wasn't entirely removed

--- default.nix
+++ default.nix
@@ -21,7 +21,7 @@
 }:
 let
   # Add libraries to the scope of callPackage
-  callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // mylib // self);
+  callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // self);
   #ocamlCallPackage = pkgs.ocamlPackages.callPackageWith (pkgs // pkgs.xlibs // self);
 
   self = rec {
