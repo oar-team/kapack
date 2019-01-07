d789e48e19d8521b8b52228df4572ab3313702ca
MERCIER Michael <michael.mercier@inria.fr>
2019-01-07 10:57:47 +0100

    evalys: 4.0.2 -> 4.0.3 fix release issues

--- evalys/evalys4.nix
+++ evalys/evalys4.nix
@@ -1,11 +1,11 @@
 {evalys, pythonPackages}:
 evalys.overrideAttrs (oldAttrs: rec {
   inherit (oldAttrs) pname;
-  version = "4.0.2";
+  version = "4.0.3";
   name = "${pname}-${version}";
 
   src = pythonPackages.fetchPypi {
     inherit pname version;
-    sha256 = "0709p54sbcy2783vmmim7rs7c12ycc4nzfpc2i3fkbwanm72bsd5";
+    sha256 = "0yywih3pv4w4ynl3b3dz9wz0qkhiqyrph5srl330wla2ycyppwb1";
   };
 })
