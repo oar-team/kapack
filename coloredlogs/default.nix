901c8e70cae5dd6e815572b1bd6faaa8bbc4cf8d
Millian Poquet <millian.poquet@inria.fr>
2019-01-16 14:55:41 +0100

    humanfriendly, coloredlogs: pypi_url->fetchPypi

--- coloredlogs/default.nix
+++ coloredlogs/default.nix
@@ -1,12 +1,12 @@
-{ stdenv, pkgs, pypi_url, pythonPackages, humanfriendly }:
+{ stdenv, pkgs, pythonPackages, humanfriendly }:
 
 pythonPackages.buildPythonPackage rec {
     pname = "coloredlogs";
-    version = "4.4.1";
+    version = "7.3";
     name = "${pname}-${version}";
 
-    src = pkgs.fetchurl {
-      url = "https://pypi.python.org/packages/b4/41/97147119b23b429e5337969c6c389eab7ef09e284d8febed936cec9d7b84/coloredlogs-7.3.tar.gz";
+    src = pythonPackages.fetchPypi {
+      inherit pname version;
       sha256 = "894641ed5e8c48e01fa0b1d62b15cd32030c6c16e1dda42f6ce5f966ac558cae";
     };
 
