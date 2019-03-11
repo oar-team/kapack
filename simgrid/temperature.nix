c4f1ec1c0dba509be04416a9cbe3e68823bb5b14
clement-dell <clement.mommessin@inria.fr>
2019-03-11 10:03:21 +0100

    New temperature plugin in Simgrid!

--- simgrid/temperature.nix
+++ simgrid/temperature.nix
@@ -3,7 +3,8 @@
 simgrid_dev_working.overrideAttrs (oldAttrs: rec {
   name = "simgrid-temperature";
 
-  patches = [ ./temperature-working.patch ];
+  #patches = [ ./temperature-working.patch ];
+  patches = [ ./temperature-multiple.patch ];
 
   #doCheck = false;
 })
