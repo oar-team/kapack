e7b2d6063e137066254a5469a08f146160d87e46
Faure Adrien <adrien.faure2@gmail.com>
2019-04-15 15:02:03 +0200

    oar_lib:init

---
+++ oar/oarlib.nix
@@ -0,0 +1,27 @@
+{stdenv, pythonPackages, procset}:
+
+pythonPackages.buildPythonPackage rec {
+  pname = "oar-lib";
+  version = "0.4.1";
+  name = "${pname}-${version}";
+
+  src = pythonPackages.fetchPypi {
+    inherit pname version;
+    sha256 = "1xpjg3jp6a8im0aipz77b5cn3mv7vqxzkzflhh4mvddb147r6pi8";
+  };
+
+  propagatedBuildInputs = with pythonPackages; [
+    alembic
+    sqlalchemy
+  ];
+
+  doCheck  = false;
+  meta = with stdenv.lib; {
+    description = "Python OAR Common Library";
+    homepage    = https://github.com/oar-team/oar3;
+    platforms   = platforms.unix;
+
+    longDescription = ''
+    '';
+  };
+}
