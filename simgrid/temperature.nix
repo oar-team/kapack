8ad5d60b4a974b0f023659afa32ae23914ab9542
clement-dell <clement.mommessin@inria.fr>
2019-05-14 09:05:30 +0200

    Pin SG version for temperature

--- simgrid/temperature.nix
+++ simgrid/temperature.nix
@@ -1,10 +1,13 @@
-{ simgrid_dev_working, fetchgit }:
+{ simgrid_dev, fetchgit }:
 
-simgrid_dev_working.overrideAttrs (oldAttrs: rec {
+simgrid_dev.overrideAttrs (oldAttrs: rec {
   name = "simgrid-temperature";
+
+  rev = "52fa9e20e056119074cd759141021286786655c5";
+  src = fetchTarball "https://github.com/simgrid/simgrid/archive/${rev}.tar.gz";
 
   #patches = [ ./temperature-working.patch ];
   patches = [ ./temperature-multiple.patch ];
 
-  #doCheck = false;
+  doCheck = false;
 })
