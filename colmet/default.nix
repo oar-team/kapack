a73902519b6f8898bfd23a47c3a2e4396b98a95b
Faure Adrien <adrien.faure2@gmail.com>
2019-08-09 15:46:02 +0200

    colmet: colmet2 to colmet3

--- colmet/default.nix
+++ colmet/default.nix
@@ -1,22 +1,45 @@
-{ stdenv, fetchFromGitHub, python27Packages, pkgs, zeromq }:
+{ stdenv, fetchFromGitHub, pkgs, pythonPackages, zeromq ,powercap }:
 
-python27Packages.buildPythonPackage rec {
+pythonPackages.buildPythonPackage rec {
 
   src = fetchFromGitHub {
-    owner = "adfaure";
+    owner = "oar-team";
     repo = "colmet";
-    rev = "88de18da67c9a24a2f3b4a5f30f735b382cfe0ff";
-    sha256 = "0hswgmjbs5yglsgmxmr3qra8zfc3w7ds3clxqihhhb96q7ppk5rg";
+    rev = "f16d9ebd830c706291dd67b4bb5a6b368dd73bb5";
+    sha256 = "0qzjn0v061n3w2pfji4d1yga2x8fs1ikxr872b2ng3q6829411ns";
   };
 
-  propagatedBuildInputs = with python27Packages; [
+  propagatedBuildInputs = with pythonPackages; [
+    requests
     pytest
     tables
     pyinotify
     pyzmq
   ];
 
-  name = "colmet-0.5.5dev1";
+  buildInputs = [ powercap ];
+  name = "colmet3dev";
+
+  # Colmet depends on some libraries located inside the repository
+  # I handle them manually
+  # 1) The patch, patches the makefiles that have a path to /usr/lib
+  # 2) The preBuild changes the backends code to link to the path of this package
+  patches = [ ./patch_makefile_backends ];
+
+  preBuild = ''
+    sed -i "s#/usr/lib/#$out/lib/#g" colmet/node/backends/perfhwstats.py
+    sed -i "s#/usr/lib/#$out/lib/#g" colmet/node/backends/RAPLstats.py
+  '';
+
+  postBuildPhase = ''
+    cd colmet/node/backends/lib_rapl; make
+    cd colmet/node/backends/lib_perf_hw; make
+  '';
+
+  postInstall = ''
+    mv colmet/node/backends/lib_rapl/lib_rapl.so $out/lib
+    mv colmet/node/backends/lib_perf_hw/lib_perf_hw.so $out/lib
+  '';
 
   # Tests do not pass
   doCheck = false;
@@ -27,9 +50,10 @@ python27Packages.buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Python library that allows you to finely manage unix processes on thousands of remote hosts";
+    broken = false;
     homepage    = https://github.com/oar-team/colmet;
     platforms   = platforms.unix;
-    licence     = licenses.gpl2;
+    license     = licenses.gpl2;
     longDescription = ''
     Colmet is a monitoring tool to collect metrics about jobs running in a
     distributed environnement, especially for gathering metrics on clusters
