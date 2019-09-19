7ed0ac6be924f8dec4544c6e66c2fcf50799130a
Millian Poquet <millian.poquet@inria.fr>
2019-09-19 18:03:54 +0200

    ompi: keep it simple

--- openmpi-rsg-plugins/default.nix
+++ openmpi-rsg-plugins/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, cmake, simgrid, remote_simgrid,
+{ clangStdenv, fetchgit, cmake, pkgconfig, simgrid, remote_simgrid,
   boost, cppzmq, glibcLocales, openmpi, jansson, which
 
 # Print btl logs?
@@ -8,8 +8,8 @@
 , printOdlsLogs ? true
 }:
 
-stdenv.mkDerivation rec {
-  name = "openmpi-rsg-plugins-${version}";
+clangStdenv.mkDerivation rec {
+  pname = "openmpi-rsg-plugins";
   version = "unreleased";
 
   src = fetchgit {
@@ -18,46 +18,26 @@ stdenv.mkDerivation rec {
     sha256 = "1yyzdv1995lizl7n85i0d2ibkxslx4cpjkncwgkkfdkjc9fhkgqw";
   };
 
-  nativeBuildInputs = [ boost cmake openmpi which ];
+  nativeBuildInputs = [ boost cmake openmpi which pkgconfig ];
   buildInputs = [ glibcLocales ];
   propagatedBuildInputs = [ remote_simgrid jansson ];
 
-  # Environment variables used by cmake (convenient for shell)
-  SimGrid_PATH = "${simgrid}";
-  RSG_INSTALL_PATH = "${remote_simgrid}";
+  # TODO: Make sure plugins call the right RSG binaries
+
+  # env variables used by CMake
   OMPI_SRC_PATH = "${openmpi}";
   OMPI_INSTALL_PATH = "${openmpi}";
 
-  # Make sure plugins call the right RSG binaries
-  patchPhase = ''
-    # Patch rsg_server binary calls
-    sed -i -E \
-      "sW(const char \* rsgserver = \")(rsg_server)(\";)W\1$(which rsg_server)\3W" \
-      src/odls/rsg/odls_rsg_module.c
-
-    # Patch rsgstat binary calls
-    sed -i -E \
-      "sW(const char \* rsgstat = \")(rsgstat)(\";)W\1$(which rsgstat)\3W" \
-      src/odls/rsg/odls_rsg_module.c
-  '';
-
-  cmakeFlags = with stdenv; ["-DCMAKE_BUILD_TYPE=Debug"]
-    ++ lib.optional printBtlLogs "-Denable_debug_btl_self=ON"
-    ++ lib.optional printOdlsLogs "-Denable_debug_odls_rsg=ON"
-  ;
-
-  installPhase = ''
-    # Put all plugins into the lib/openmpi/ directory
-    mkdir -p $out/lib/openmpi
-
-    find . -name '*.so' | sed -E 'sW(.*)Wcp \1 $out/lib/openmpi/W' | bash -eux
-  '';
+  cmakeFlags = with clangStdenv; ["-DCMAKE_BUILD_TYPE=Debug"];
 
   enableParallelBuilding = true;
 
-  meta = with stdenv.lib; {
+  meta = with clangStdenv.lib; {
     description = "OpenMPI plugins to use Remote SimGrid as network backend.";
+    longDescription = description;
     homepage    = "https://gitlab.inria.fr/mpoquet/openmpi-rsg-plugins";
     platforms   = platforms.unix;
+    license = licenses.lgpl3;
+    broken = false;
   };
 }
--- openmpi-rsg/default.nix
+++ openmpi-rsg/default.nix
@@ -1,13 +1,12 @@
-{ stdenv, openmpi, openmpi_rsg_plugins, python, rsync }:
+{ stdenv, openmpi, openmpi_rsg_plugins, rsync }:
 
 stdenv.mkDerivation rec {
-# name = "openmpi-3.1.1";
-  name = "openmpi-rsg-u"; # BEWARE: Length must equal openmpi package's
+  name = "openmpi-rsg";
 
   # This package has no source. It's just a mix of OpenMPI + plugins
   phases = [ "buildPhase" "installPhase" ];
 
-  nativeBuildInputs = [ python rsync ];
+  nativeBuildInputs = [ rsync ];
   buildInputs = [ openmpi openmpi_rsg_plugins ];
 
   buildPhase = ''
@@ -15,65 +14,6 @@ stdenv.mkDerivation rec {
 
     echo "Copy OpenMPI files into the build directory"
     rsync -a ${openmpi}/* $TMP/out --exclude ompi --exclude orte --exclude opal
-
-    echo "Hack the hardcoded install path of the OpenMPI binaries"
-    # This is done in python, for sanity's sake
-    export BUILDDIR=$TMP
-    export OPENMPI=${openmpi}
-    export OUT=$out
-
-    python_cmd=$(cat <<-END
-    import glob
-    import os
-    import sys
-    from pathlib import Path
-    from stat import *
-
-    builddir_path = os.environ['BUILDDIR']
-    openmpi_path = os.environ['OPENMPI']
-    out_path = os.environ['OUT']
-
-    if len(openmpi_path) != len(out_path):
-        raise Exception("OpenMPI path ({}) and out path ({}) should have equal length".format(
-            openmpi_path, out_path))
-
-    files_to_hack = glob.glob(builddir_path + "/out/**", recursive=True)
-
-    for filename in files_to_hack:
-        # Only work on files
-        if not os.path.isfile(filename):
-            continue
-        if os.path.islink(filename):
-            continue
-
-        # Mark the file as writable if needed
-        old_mode = os.stat(filename)[ST_MODE]
-        changed_mode = False
-        if old_mode & S_IWUSR == 0:
-            changed_mode = True
-            os.chmod(filename, old_mode | S_IWUSR)
-
-        # print('hack OpenMPI path in {}'.format(filename, openmpi_path,
-        #     out_path))
-
-        # Regular (non-directory, non-links) files
-        with open(filename, 'rb+') as f:
-            # Just read the content
-            content = f.read()
-
-            # Hack the path into the file content
-            content = content.replace(openmpi_path.encode('ascii'),
-                out_path.encode('ascii'))
-
-            # Update content
-            f.seek(0, 0)
-            f.write(content)
-
-        if changed_mode:
-            os.chmod(filename, old_mode)
-    END
-    )
-    python -c "$python_cmd"
 
     echo "Copy plugins into the build directory"
     rsync -a ${openmpi_rsg_plugins}/lib/* $TMP/out/lib
@@ -83,4 +23,13 @@ stdenv.mkDerivation rec {
     echo "Move the temporary directory into the nix store"
     rsync -a $TMP/out/* $out
   '';
+
+  meta = with stdenv.lib; {
+    description = "OpenMPI with Remote SimGrid plugins.";
+    homepage = "none";
+    longDescription = description;
+    license = licenses.bsd3;
+    platforms   = platforms.unix;
+    broken = false;
+  };
 }
